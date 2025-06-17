c40a146f6eef6ed327bf0029d7ada4ec276e311f
Matthew B White <matthew@mh-white.com>
2025-06-17 20:35:55 +0000

    updates
    Signed-off-by: Matthew B White <matthew@mh-white.com>

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using MailKit.Net.Smtp;
+using MailKit;
+using MimeKit;
+
+
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("a1");
+            var message = new MimeMessage ();
+            message.From.Add (new MailboxAddress ("Alice", "alice@ampretia.co.uk"));
+            message.To.Add (new MailboxAddress ("MBW", "matthew@mh-white.com"));
+            message.Subject = "How you doin'?";
+
+            message.Body = new TextPart ("plain") {
+                Text = @"Hey Chandler,
+
+I just wanted to let you know that Monica and I were going to go play some paintball, you in?
+
+-- Joey"
+            };
+
+            using (var client = new SmtpClient ()) {
+                client.Connect ("mail.ampretia.co.uk", 465, true);
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate ("alice@ampretia.co.uk", "passw0rdWibble");
+
+                client.Send (message);
+                client.Disconnect (true);
+            }
+        }
+    }
